3025ccc410dd500e15930ba2fb632bcee613e5e1
Yohanes Naibaho <yohanespratama303@gmail.com>
2023-12-02 09:35:39 +0700

    add route for crud

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
@@ -28,6 +28,76 @@ app.post("/books", async (request, respone) => {
     };
 
     const book = await Book.create(newBook);
+  } catch (error) {
+    console.log(error.message);
+    respone.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Books from database
+app.get("/books", async (request, respone) => {
+  try {
+    const books = await Book.find({});
+
+    return respone.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    respone.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get One Books from database by ID
+app.get("/books/:id", async (request, respone) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return respone.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    respone.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Update a Book
+app.put("/books/:id", async (request, respone) => {
+  try {
+    if (!request.body.title || !request.body.author || !request.body.title) {
+      return respone.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return respone.status(404).json({ message: "Book Not Found" });
+    }
+
+    return respone.status(200).send({ message: "Book Updated Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    respone.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Delete a Book
+app.delete("/books/:id", async (request, respone) => {
+  try {
+    const { id } = request.params;
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return respone.status(404).json({ message: "Book Not Found" });
+    }
+
+    return respone.status(200).send({ message: "Book Deleted Successfully" });
   } catch (error) {
     console.log(error.message);
     respone.status(500).send({ message: error.message });
